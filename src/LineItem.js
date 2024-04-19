1096b60c6d005d608c61c645f3d3b788069cc53b
AKSHUNNA-DHAKAD <115500257+AKSHUNNA-DHAKAD@users.noreply.github.com>
2024-04-19 23:57:02 +0530

    Add files via upload

---
+++ src/LineItem.js
@@ -0,0 +1,23 @@
+import { FaTrashAlt } from 'react-icons/fa';
+
+const LineItem = ({ item, handleCheck, handleDelete }) => {
+    return (
+        <li className="item">
+            <input
+                type="checkbox"
+                onChange={() => handleCheck(item.id)}
+                checked={item.checked}
+            />
+            <label
+                style={(item.checked) ? { textDecoration: 'line-through' } : null}
+                onDoubleClick={() => handleCheck(item.id)}
+            >{item.item}</label>
+<button onClick={() => handleDelete(item.id)}>Delete</button>
+
+        </li>
+    )
+}
+
+export default LineItem
+
+
